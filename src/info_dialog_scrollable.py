62ed7644289f8152bc3c4c50cbd20bddee6db498
ijgnd <does_not_exist@example.com>
2022-07-18 09:04:45 +0200

    add ok button to info box

--- src/info_dialog_scrollable.py
+++ src/info_dialog_scrollable.py
@@ -1,8 +1,10 @@
 from aqt.qt import (
     QDialog,
+    QDialogButtonBox,
     Qt,
     QVBoxLayout,
     QTextBrowser,
+    qconnect,
     qtmajor,
 )
 from aqt.utils import openLink
@@ -18,12 +20,18 @@ class InfoDialog(QDialog):
         self.setWindowTitle(window_title)
         self.vbox = QVBoxLayout()
         self.vbox.setContentsMargins(0, 0, 0, 0)
+
         self.te = QTextBrowser()
         self.te.setOpenLinks(False)  # TextBrowser should not open links in its window - https://doc.qt.io/qt-6/qtextbrowser.html#openLinks-prop
         self.te.anchorClicked.connect(self.open_links)  # I need no_bundled_libs() ....
         self.te.setHtml(content)
         self.te.setReadOnly(True)
         self.vbox.addWidget(self.te)
+
+        self.button_box = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok)
+        qconnect(self.button_box.button(QDialogButtonBox.StandardButton.Ok).clicked, self.accept)
+        self.vbox.addWidget(self.button_box)
+
         self.setLayout(self.vbox)
         self.resize(950, 480)
     
