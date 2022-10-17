aac392477e1dd5198b3d552d3ed85ca8b8778e7b
ijgnd <does_not_exist@example.com>
2022-10-17 16:38:51 +0200

    pdfium more useless code for inverting colors

--- src/pdf.py
+++ src/pdf.py
@@ -225,6 +225,58 @@ const css = `
 `
 overlay.setAttribute("style", css);
 document.body.appendChild(overlay);
+
+// https://stackoverflow.com/a/21581690 - load jquery
+// Immediately-invoked function expression
+(function() {
+  // Load the script
+  const script = document.createElement("script");
+  script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
+  script.type = 'text/javascript';
+  script.addEventListener('load', () => {
+    console.log(`jQuery ${$.fn.jquery} has been loaded successfully!`);
+    // use jQuery below
+  });
+  document.head.appendChild(script);
+})();
+
+
+// manually replace colors with jquery - https://stackoverflow.com/a/30724171
+// this breaks the css hack
+function colorReplace(findHexColor, replaceWith) {
+  // Convert rgb color strings to hex
+  // REF: https://stackoverflow.com/a/3627747/1938889
+  function rgb2hex(rgb) {
+    if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    function hex(x) {
+      return ("0" + parseInt(x).toString(16)).slice(-2);
+    }
+    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+  }
+
+  // Select and run a map function on every tag
+  $('*').map(function(i, el) {
+    // Get the computed styles of each tag
+    var styles = window.getComputedStyle(el);
+
+    // Go through each computed style and search for "color"
+    Object.keys(styles).reduce(function(acc, k) {
+      var name = styles[k];
+      var value = styles.getPropertyValue(name);
+      if (value !== null && name.indexOf("color") >= 0) {
+        // Convert the rgb color to hex and compare with the target color
+        if (value.indexOf("rgb(") >= 0 && rgb2hex(value) === findHexColor) {
+          // Replace the color on this found color attribute
+          $(el).css(name, replaceWith);
+        }
+      }
+    });
+  });
+}
+
+// Call like this for each color attribute you want to replace
+//colorReplace("#ada9a6", "#21222c");
 """
         self.webview.page().runJavaScript(js)
 
